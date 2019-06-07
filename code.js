72a068f259745309250756825b9f0c9fca34e20d
Nate Kinne <nathan.kinne@gmail.com>
2019-06-07 10:51:27 -0500

    deploy

--- code.js
+++ code.js
@@ -7,9 +7,6 @@ function pingApi(term){
 }
 // display results for searched handle
 function displayResults(response){
-    console.log(response.fullname);
-    
-    console.log(response);
     $('.not-found').addClass('hidden');
     // if the handle isnt found
     if(response.message === "Not Found"){
@@ -17,25 +14,21 @@ function displayResults(response){
     }else{
         $(".found").show();
         response.forEach(function (repo) {
-            console.log(repo.html_url);
-            $(".found").append(`<div class="results">
-        <p>${repo.full_name}</p>
-        <a href='${repo.html_url}' target="_blank">Repo Link</a>
-        </div>`);
-            console.log('finished');
+            
+        $(".found").append(`<div class="results">
+            <p>${repo.full_name}</p>
+            <a href='${repo.html_url}' target="_blank">Repo Link</a>
+            </div>`);
         });
     }
-    
-    
+      
 }
 
 function watchForm(){
-    console.log("awaiting instructions");
     $(document).on('submit', 'form', function(e){
         e.preventDefault();
         $('.results').remove();
         let term = $('#search-term').val();
-        console.log(term);
         pingApi(term);
     });
 }
